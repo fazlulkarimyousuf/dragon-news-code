43b47d38ca5bf8dea15babfbc5b4b3bcebc35b53
fazlulkarimyousuf <faizmiah52@gmail.com>
2025-05-24 06:15:21 +0600

    latest news section done

--- src/Components/LatesNews.jsx
+++ src/Components/LatesNews.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import Marquee from "react-fast-marquee";
 
 const LatesNews = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="flex items-center gap-5 bg-base-200 p-3">
+      <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
+      <Marquee className="flex gap-5" pauseOnHover={true} speed={60}>
+        <p className="font-bold px-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
+          inventore.
+        </p>
+          <p className="font-bold px-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
+        inventore.
+      </p>
+        <p className="font-bold px-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
+        inventore.
+      </p>
+        <p className="font-bold px-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
+        inventore.
+      </p>
+      </Marquee>
+    </div>
+  );
 };
 
 export default LatesNews;
